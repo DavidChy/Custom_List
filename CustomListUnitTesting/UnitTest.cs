95ad46fcba70e2060267497748d12de98c0256a3
David <david.chy.usmc@gmail.com>
2018-11-07 14:36:49 -0600

    All 5 Add Test Methods successful. Moving onto Remove.

--- CustomListUnitTesting/UnitTest.cs
+++ CustomListUnitTesting/UnitTest.cs
@@ -5,73 +5,112 @@ using CustomList;
 namespace CustomListUnitTest
 {
     [TestClass]
-    public class AddTestMethod
+    public class UnitTest
     {
 
         [TestMethod]
         public void AddTestMethod1()
         {
-            //Arrange
+            // Arrange
             CustomList<int> list = new CustomList<int>();
             int expected = 1;
 
-            //Act
+            // Act
             list.Add(16);
 
-            //Assert
+            // Assert
             Assert.AreEqual(expected, list.Count);
         }
 
         [TestMethod]
         public void AddTestMethod2()
         {
+            // Arrange
             CustomList<int> list = new CustomList<int>();
             int expected = 17;
 
+            // Act
             list.Add(16);
             list.Add(17);
 
+            // Assert
             Assert.AreEqual(expected, list[1]);
         }
 
         [TestMethod]
         public void AddTestMethod3()
         {
+            // Arrange
             CustomList<int> list = new CustomList<int>();
             int expected = 16;
 
+            // Act
             list.Add(16);
             list.Add(17);
 
+            // Assert
             Assert.AreEqual(expected, list[0]);
         }
 
         [TestMethod]
-        public void AddVariable_CheckIndex0_ReturnsSameVariable()
+        public void AddTestMethod4()
         {
-            //Arrange
-            CustomList<int> myList = new CustomList<int>();
-            int value = 10;
+            // Arrange
+            CustomList<int> list = new CustomList<int>();
+            int expected = 18;
 
-            //Act
-            myList.Add(value);
+            // Act
+            list.Add(16);
+            list.Add(17);
+            list.Add(18);
 
-            //Assert
-            Assert.AreEqual(value, myList[0]);
+            // Assert
+            Assert.AreEqual(expected, list[2]);
         }
 
         [TestMethod]
-        public void Indexer_GetValue_ReturnGetValue()
+        public void AddTestMethod5()
         {
-            //Arrange
-            CustomList<int> testList = new CustomList<int>();
-            int i = 0;
+            // Arrange
+            CustomList<int> list = new CustomList<int>();
+            int expected = 40;
 
-            //Act
-            testList.Add(1);
+            // Act
+            list.Add(16);
+            list.Add(17);
+            list.Add(18);
+            list.Add(40);
 
-            //Assert
-            Assert.AreEqual(1, testList[i]);
+            // Assert
+            Assert.AreEqual(expected, list[3]);
         }
+
+        [TestMethod]
+        public void AddValue_CheckIndex0_ReturnsSameValue()
+        {
+            // Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 10;
+
+            // Act
+            myList.Add(value);
+
+            // Assert
+            Assert.AreEqual(value, myList[0]);
+        }
+
+        //[TestMethod]
+        //public void Indexer_GetValue_ReturnGetValue()
+        //{
+        //    // Arrange
+        //    CustomList<int> testList = new CustomList<int>();
+        //    int i = 0;
+
+        //    //Act
+        //    testList.Add(1);
+
+        //    //Assert
+        //    Assert.AreEqual(1, testList[i]);
+        //}
     }
 }
